954bee9bea149e38e7bbde7e620b5bf346486d69
Ilian <iliyan@ubuntu.ubuntu-domain>
2014-04-22 22:28:36 +0300

    changing card.h

--- Manapool/card.h
+++ Manapool/card.h
@@ -8,7 +8,12 @@ struct card_t {
     int hp;
 };
 
+//printf card: name(cost) (atk|hp)
 void print_card(struct card_t card); 
+
+//returns 0 if both cards are death
+//returns 1 if the attacker won returns 2 if the defender won
+//returns 3 if both cards are alive
 int attack(struct card_t *attacker, struct card_t *defender);
 
 #endif
